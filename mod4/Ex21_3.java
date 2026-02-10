0e9b22438d309d2a2c5dfb5d783dac3003b51ce8
griegel-ivt <griegel@ivytech.edu>
2026-02-10 17:36:08 -0500

    Ex 21.3 + testfile changes

---
+++ mod4/Ex21_3.java
@@ -0,0 +1,89 @@
+package mod4;
+
+import java.io.*;
+import java.util.*;
+
+public class Ex21_3 {
+    public static void main(String[] args) throws Exception {  
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Java source file: ");
+        String filename = input.nextLine();
+
+        File file = new File(filename);
+        if (file.exists()) {
+            System.out.println("The number of keywords in " + filename 
+            + " is " + countKeywords(file));
+        }
+        else {
+            System.out.println("File " + filename + " does not exist");
+        }    
+    }
+
+    public static int countKeywords(File file) throws Exception {  
+        // Array of all Java keywords + true, false and null
+        String[] keywordString = {"abstract", "assert", "boolean", 
+        "break", "byte", "case", "catch", "char", "class", "const",
+        "continue", "default", "do", "double", "else", "enum",
+        "extends", "for", "final", "finally", "float", "goto",
+        "if", "implements", "import", "instanceof", "int", 
+        "interface", "long", "native", "new", "package", "private",
+        "protected", "public", "return", "short", "static", 
+        "strictfp", "super", "switch", "synchronized", "this",
+        "throw", "throws", "transient", "try", "void", "volatile",
+        "while", "true", "false", "null"};
+
+        Set<String> keywordSet = new HashSet<>(Arrays.asList(keywordString));
+        String code = "";
+        try (Scanner input = new Scanner(file)) {
+            while (input.hasNextLine()) {
+                code += input.nextLine() + "\n";
+            }
+        }
+
+        int count = 0;
+        boolean inString = false;
+        boolean inComment = false;
+        String currentWord = "";
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+            char nextC;
+            if (i + 1 < code.length()) {
+                nextC = code.charAt(i + 1);
+            } else {
+                nextC = ' ';
+            }
+
+            if (!inString && !inComment) {
+                if (c == '"') {
+                    inString = true;
+                    continue;
+                }
+                else if (c == '/' && nextC == '/') {
+                    inComment = true;
+                    i++;
+                    continue;
+                }
+            }
+            else if (inString && c == '"') {
+                inString = false;
+                continue;
+            }
+            else if (inComment && c == '\n') {
+                inComment = false;
+                continue;
+            }
+            
+            if (!inString && !inComment) {
+                if (Character.isLetter(c)) {
+                    currentWord += c;
+                } else {
+                    if (currentWord.length() > 0) {
+                        if (keywordSet.contains(currentWord)) count++;
+                        currentWord = "";
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
--- mod4/testfile.java
+++ mod4/testfile.java
@@ -2,6 +2,7 @@ package mod4;
 
 public class testfile {
     public static void main(String[] args) {
-        System.out.println("Testing file is working!");
-    }
+        System.out.println("Testing file is finally working!" +
+        "Sometimes I want to throw a package at Java."); // This is a comment!
+    } // I should try to catch a break.
 }
