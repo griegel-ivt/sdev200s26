af3920d74e76e162a63d9e434fb93d24826c846d
griegel-ivt <griegel@ivytech.edu>
2026-02-10 18:09:20 -0500

    Midterm stuff

---
+++ midterm/PrintMonth2/src/TestDaysOfWeek.java
@@ -0,0 +1,13 @@
+import daysAndDates.DaysOfWeek;
+
+public class TestDaysOfWeek {
+
+	public static void main(String[] args) {
+		System.out.println("Days Of Week: ");
+		for (int i = 1;i < 8;i++) {
+			System.out.println("Number: " + i + "\tDay Of Week: " + DaysOfWeek.DayOfWeekStr(i) );
+		}
+
+	}
+
+}
